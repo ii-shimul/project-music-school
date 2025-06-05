67718df7b9436601447a4d16df366a72c3a4b45f
ii-shimul <islamshimul27@gmail.com>
2025-06-06 01:33:30 +0600

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <main className="mih-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1>helooooooo</h1>
+      <HeroSection/>
     </main>
   );
 }
